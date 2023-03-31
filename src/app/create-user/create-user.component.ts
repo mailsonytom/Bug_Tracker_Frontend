2428fee08eab1e1776202f9b5a73f27a2ad041e7
User <nidhu@example.com>
2023-03-31 15:31:57 +0530

    extracting role for dd.
    role for dd

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent {
+  constructor(private http: HttpClient){}
+
+  userDepartments: any;
+  UserUnderDeptArray: any[] = [];
+
   userForm = new FormGroup({
     displayName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.maxLength(40), Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
@@ -15,6 +20,32 @@ export class CreateUserComponent {
     isActive: new FormControl(true),
     gender: new FormControl('', [Validators.required])
   });
+  ngOnInit(): void {
+
+    this.getUsersForDept();
+  }
+
+
+  // public getUsersForDept() {
+  //   this.http.get('http://localhost:4000/user').subscribe((response: any) => {
+  //     let DeptArray: any[] = [];
+  //     response.users.map((resObj: any) => {
+  //       DeptArray.push(resObj.role)
+  //     })
+  //     this.userDepartments = Array.from(new Set(DeptArray));
+  //   });
+  // }
+  public getUsersForDept() {
+    this.http.get('http://localhost:4000/user').subscribe((response: any) => {
+      let DeptArray: any[] = [];
+      response.users.map((resObj: any) => {
+        DeptArray.push(resObj.role);
+      });
+      this.userDepartments = DeptArray.filter((role, index, array) => {
+        return array.indexOf(role) === index;
+      });
+    });
+  }
   
 
   onSubmit() {
