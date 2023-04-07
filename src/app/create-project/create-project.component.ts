4441adc0f7c51d9fc3aa5a475336f96c6284ffd5
User <nidhu@example.com>
2023-04-07 11:25:14 +0530

    Upadated routing,projectid,css

--- src/app/create-project/create-project.component.ts
+++ src/app/create-project/create-project.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
@@ -8,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CreateProjectComponent {
   projectForm = new FormGroup({
-    id: new FormControl('', [Validators.required]),
+    // id: new FormControl('', [Validators.required]),
     name: new FormControl('', [Validators.required]),
     primaryOwner: new FormControl('', [Validators.required]),
     secondaryOwner: new FormControl('', [Validators.required]),
@@ -16,16 +18,23 @@ export class CreateProjectComponent {
     description: new FormControl('')
   });
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private router: Router) {}
 
+  // 
   onSubmit() {
+    
     if (this.projectForm.valid) {
       const formData = this.projectForm.value;
       console.log(formData);
       this.http.post('http://localhost:4000/project', formData).subscribe({
         next: (response: any) => {
           console.log(response); // Handle success response
-          alert('Project created successfully!');
+          const confirmRedirect = confirm('Project created successfully! Do you want to view the project?');
+          if (confirmRedirect) {
+            this.router.navigate(['/projects']); // Redirect to view-project page 
+          } else {
+            this.router.navigate(['/home']); // Redirect to home page
+          }
         },
         error: (error) => {
           console.log(error); // Handle error response
@@ -35,6 +44,7 @@ export class CreateProjectComponent {
       this.projectForm.markAllAsTouched();
     }
   }
+  
 
   onCancel() {
     this.projectForm.reset();
--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent {
-  constructor(private http: HttpClient){}
+  
+  constructor(private http: HttpClient,private router: Router){}
 
   userDepartments: any;
   UserUnderDeptArray: any[] = [];
@@ -43,13 +45,22 @@ export class CreateUserComponent {
       const formData = this.userForm.value;
       console.log(formData); // TODO: Replace with actual code to send data to server
       this.http.post('http://localhost:4000/user', formData).subscribe((response: any) => {
-        console.log("Post response", response)
-        alert('User created successfully!');
-      })
+        console.log("Post response", response);
+        const confirmRedirect = confirm('User created successfully!\nDo you want to redirect to the view user page?\nClick no if you want to redirect to home page');
+        if (confirmRedirect) {
+          // Redirect to view user page
+          // Assuming the view user component is named ViewUserComponent
+          this.router.navigate(['/users']);
+        } else {
+          // Redirect to home page
+          this.router.navigate(['/home']);
+        }
+      });
     } else {
       this.userForm.markAllAsTouched();
     }
   }
+  
 
   onCancel() {
     this.userForm.reset();
